bf68b07f8f305319cf85cbbd80a1e66a427bf98e
zak <me@my.com>
2025-09-04 16:31:29 -0500

    comitting new changes to serv and bios

---
+++ serv/react-1/src/App.jsx
@@ -0,0 +1,13 @@
+// App.js
+import Home from './components/Home/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
